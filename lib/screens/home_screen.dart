ee0b17d9eda9cf656a2aa178220ff6bcbba333f0
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-03 14:32:00 -0300

    handling controller

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,12 @@
+import 'package:crypto/controllers/controllers.dart';
 import 'package:crypto/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final CoinController controller = Get.put(CoinController());
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,87 +28,89 @@ class HomeScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 10,
-                  itemBuilder: (_, __) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 15.0),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: 60,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  width: 60,
-                                  height: 60,
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey[700],
-                                      borderRadius: BorderRadius.circular(15),
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: Colors.grey[700]!,
-                                            offset: const Offset(4, 4),
-                                            blurRadius: 5)
-                                      ]),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Image.network(
-                                        'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'),
-                                  ),
-                                ),
-                                const SizedBox(width: 20),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      'Bitcoin',
-                                      style: textStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                Obx(
+                  () => ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 10,
+                    itemBuilder: (_, __) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 15.0),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey[700],
+                                        borderRadius: BorderRadius.circular(15),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.grey[700]!,
+                                              offset: const Offset(4, 4),
+                                              blurRadius: 5)
+                                        ]),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Image.network(
+                                          'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'),
                                     ),
-                                    Text(
-                                      '10%',
-                                      style: textStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
+                                  ),
+                                  const SizedBox(width: 20),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(height: 10),
+                                      Text(
+                                        'Bitcoin',
+                                        style: textStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                const SizedBox(height: 10),
-                                Text(
-                                  '\$ 200',
-                                  style: textStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                                      Text(
+                                        '10%',
+                                        style: textStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                ),
-                                Text(
-                                  'BI',
-                                  style: textStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    '\$ 200',
+                                    style: textStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                )
-                              ],
-                            )
-                          ],
+                                  Text(
+                                    'BI',
+                                    style: textStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 )
               ],
             ),
