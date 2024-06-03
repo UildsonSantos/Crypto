0d3c3de69da8e9f45469c123f486d6468f41df3f
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-03 00:24:02 -0300

    crypto detail ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,9 +29,80 @@ class HomeScreen extends StatelessWidget {
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: 10,
                   itemBuilder: (_, __) {
-                    return const Padding(
-                      padding: EdgeInsets.only(bottom: 15.0),
-                      child: SizedBox(),
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 60,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[700],
+                                      borderRadius: BorderRadius.circular(15),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey[700]!,
+                                            offset: const Offset(4, 4),
+                                            blurRadius: 5)
+                                      ]),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Image.network(
+                                        'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'),
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      'Bitcoin',
+                                      style: textStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      '10%',
+                                      style: textStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const SizedBox(height: 10),
+                                Text(
+                                  '\$ 200',
+                                  style: textStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'BI',
+                                  style: textStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     );
                   },
                 )
