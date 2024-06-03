3ef45e0830f52118580cd0f29387931f55dc71d4
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-03 11:17:01 -0300

    coin models

---
+++ lib/models/models.dart
@@ -0,0 +1,217 @@
+import 'dart:convert';
+
+class Coin {
+    final String id;
+    final String symbol;
+    final String name;
+    final String image;
+    final double currentPrice;
+    final int marketCap;
+    final int marketCapRank;
+    final double totalVolume;
+    final double high24H;
+    final double low24H;
+    final double priceChange24H;
+    final double priceChangePercentage24H;
+    final double marketCapChange24H;
+    final double marketCapChangePercentage24H;
+    final double circulatingSupply;
+    final double ath;
+    final double athChangePercentage;
+    final DateTime athDate;
+    final double atl;
+    final double atlChangePercentage;
+    final DateTime atlDate;
+    final Roi? roi;
+
+    Coin({
+        required this.id,
+        required this.symbol,
+        required this.name,
+        required this.image,
+        required this.currentPrice,
+        required this.marketCap,
+        required this.marketCapRank,
+        required this.totalVolume,
+        required this.high24H,
+        required this.low24H,
+        required this.priceChange24H,
+        required this.priceChangePercentage24H,
+        required this.marketCapChange24H,
+        required this.marketCapChangePercentage24H,
+        required this.circulatingSupply,
+        required this.ath,
+        required this.athChangePercentage,
+        required this.athDate,
+        required this.atl,
+        required this.atlChangePercentage,
+        required this.atlDate,
+        required this.roi,
+    });
+
+    Coin copyWith({
+        String? id,
+        String? symbol,
+        String? name,
+        String? image,
+        double? currentPrice,
+        int? marketCap,
+        int? marketCapRank,
+        double? totalVolume,
+        double? high24H,
+        double? low24H,
+        double? priceChange24H,
+        double? priceChangePercentage24H,
+        double? marketCapChange24H,
+        double? marketCapChangePercentage24H,
+        double? circulatingSupply,
+        double? ath,
+        double? athChangePercentage,
+        DateTime? athDate,
+        double? atl,
+        double? atlChangePercentage,
+        DateTime? atlDate,
+        Roi? roi,
+    }) => 
+        Coin(
+            id: id ?? this.id,
+            symbol: symbol ?? this.symbol,
+            name: name ?? this.name,
+            image: image ?? this.image,
+            currentPrice: currentPrice ?? this.currentPrice,
+            marketCap: marketCap ?? this.marketCap,
+            marketCapRank: marketCapRank ?? this.marketCapRank,
+            totalVolume: totalVolume ?? this.totalVolume,
+            high24H: high24H ?? this.high24H,
+            low24H: low24H ?? this.low24H,
+            priceChange24H: priceChange24H ?? this.priceChange24H,
+            priceChangePercentage24H: priceChangePercentage24H ?? this.priceChangePercentage24H,
+            marketCapChange24H: marketCapChange24H ?? this.marketCapChange24H,
+            marketCapChangePercentage24H: marketCapChangePercentage24H ?? this.marketCapChangePercentage24H,
+            circulatingSupply: circulatingSupply ?? this.circulatingSupply,
+            ath: ath ?? this.ath,
+            athChangePercentage: athChangePercentage ?? this.athChangePercentage,
+            athDate: athDate ?? this.athDate,
+            atl: atl ?? this.atl,
+            atlChangePercentage: atlChangePercentage ?? this.atlChangePercentage,
+            atlDate: atlDate ?? this.atlDate,
+            roi: roi ?? this.roi,
+        );
+
+    factory Coin.fromRawJson(String str) => Coin.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory Coin.fromJson(Map<String, dynamic> json) => Coin(
+        id: json["id"],
+        symbol: json["symbol"],
+        name: json["name"],
+        image: json["image"],
+        currentPrice: json["current_price"]?.toDouble(),
+        marketCap: json["market_cap"],
+        marketCapRank: json["market_cap_rank"],
+        totalVolume: json["total_volume"]?.toDouble(),
+        high24H: json["high_24h"]?.toDouble(),
+        low24H: json["low_24h"]?.toDouble(),
+        priceChange24H: json["price_change_24h"]?.toDouble(),
+        priceChangePercentage24H: json["price_change_percentage_24h"]?.toDouble(),
+        marketCapChange24H: json["market_cap_change_24h"]?.toDouble(),
+        marketCapChangePercentage24H: json["market_cap_change_percentage_24h"]?.toDouble(),
+        circulatingSupply: json["circulating_supply"]?.toDouble(),
+        ath: json["ath"]?.toDouble(),
+        athChangePercentage: json["ath_change_percentage"]?.toDouble(),
+        athDate: DateTime.parse(json["ath_date"]),
+        atl: json["atl"]?.toDouble(),
+        atlChangePercentage: json["atl_change_percentage"]?.toDouble(),
+        atlDate: DateTime.parse(json["atl_date"]),
+        roi: json["roi"] == null ? null : Roi.fromJson(json["roi"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "symbol": symbol,
+        "name": name,
+        "image": image,
+        "current_price": currentPrice,
+        "market_cap": marketCap,
+        "market_cap_rank": marketCapRank,
+        "total_volume": totalVolume,
+        "high_24h": high24H,
+        "low_24h": low24H,
+        "price_change_24h": priceChange24H,
+        "price_change_percentage_24h": priceChangePercentage24H,
+        "market_cap_change_24h": marketCapChange24H,
+        "market_cap_change_percentage_24h": marketCapChangePercentage24H,
+        "circulating_supply": circulatingSupply,
+        "ath": ath,
+        "ath_change_percentage": athChangePercentage,
+        "ath_date": athDate.toIso8601String(),
+        "atl": atl,
+        "atl_change_percentage": atlChangePercentage,
+        "atl_date": atlDate.toIso8601String(),
+        "roi": roi?.toJson(),
+    };
+}
+
+class Roi {
+    final double times;
+    final Currency currency;
+    final double percentage;
+
+    Roi({
+        required this.times,
+        required this.currency,
+        required this.percentage,
+    });
+
+    Roi copyWith({
+        double? times,
+        Currency? currency,
+        double? percentage,
+    }) => 
+        Roi(
+            times: times ?? this.times,
+            currency: currency ?? this.currency,
+            percentage: percentage ?? this.percentage,
+        );
+
+    factory Roi.fromRawJson(String str) => Roi.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory Roi.fromJson(Map<String, dynamic> json) => Roi(
+        times: json["times"]?.toDouble(),
+        currency: currencyValues.map[json["currency"]]!,
+        percentage: json["percentage"]?.toDouble(),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "times": times,
+        "currency": currencyValues.reverse[currency],
+        "percentage": percentage,
+    };
+}
+
+enum Currency {
+    btc,
+    eth,
+    usd
+}
+
+final currencyValues = EnumValues({
+    "btc": Currency.btc,
+    "eth": Currency.eth,
+    "usd": Currency.usd
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    late Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+            reverseMap = map.map((k, v) => MapEntry(v, k));
+            return reverseMap;
+    }
+}
